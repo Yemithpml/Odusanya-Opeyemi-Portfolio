3911093b3fd1bb4a1dcf9e8cc520c452a1adc70a
Yemi.dev <opeyemiodusanya60@gmail.com>
2025-12-23 14:35:02 -0800

    Projects data

--- src/app/data/projects.ts
+++ src/app/data/projects.ts
@@ -2,11 +2,38 @@ const projects = [
   {
     title: "Big-Bang Store",
     description:
-      "An e-commerce website for premium headphones built with React and Tailwind CSS.",
+      "An e-commerce website having all necessary features of a real store, it displays premium headphones. Built with React and Tailwind CSS.",
     image: "/bigbang.png",
     tags: ["React", "Tailwind", "E-commerce"],
+    live: "https://big-bang-store.vercel.app/",
+    github: "https://github.com/yemithpml/big-bang-store",
+  },
+  {
+    title: "Virco Movies",
+    description:
+      "Virco is a Movie App that uses the TDMB api to fetch available movies and displays them dynamically.Built with React Js and tailwind css.",
+    image: "/virco.png",
+    tags: ["React", "Tailwind", "Movie app"],
+    live: "https://virco-movie-app.vercel.app/",
+    github: "https://github.com/yemithpml/Virco-Movie-App",
+  },
+  {
+    title: "Keeper Notes",
+    description:
+      "Keeper is a simple version of the Keeper app that allows users to add, delete notes and manage state dynamically.. Built with React Js and Tailwind CSS.",
+    image: "/Keeper.png",
+    tags: ["React", "Tailwind", "Notes app"],
     live: "https://bigbang-store.vercel.app",
-    github: "https://github.com/opeyemi/bigbang-store",
+    github: "https://github.com/yemithpml/Keeper-Notes",
+  },
+  {
+    title: "ClimaX",
+    description:
+      "ClimaX is a simple weather app that uses the free weather map api to fetch real time weather coditions. built with Html 5, css3 and vanilla javascript..",
+    image: "/climaX.png",
+    tags: ["React", "Tailwind", "weather app"],
+    live: "https://clima-x-zeta.vercel.app/",
+    github: "https://github.com/yemithpml/climaX",
   },
 ]
 
