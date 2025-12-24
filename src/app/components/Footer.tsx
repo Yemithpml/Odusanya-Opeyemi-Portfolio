e1469e72a140ea989879698fbb639569ec22442a
Yemi.dev <opeyemiodusanya60@gmail.com>
2025-12-24 06:22:08 -0800

    Footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,3 +1,9 @@
-export default function Footer() {
-  return <footer>© 2025 My Portfolio</footer>;
+function Footer() {
+  return <footer>
+    <div className="max-w-7xl mx-auto px-6 md:px-12 py-8 border-t text-center text-sm text-gray-500">
+        &copy; {new Date().getFullYear()} Opeyemi Odusanya. All rights reserved.
+    </div>
+  </footer>;
 }
+
+export default Footer
